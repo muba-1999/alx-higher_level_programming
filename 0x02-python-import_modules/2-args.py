40c3a79187e7b1b9a132dbede10aa6789a1602e0
Mubanga <mukobam4@gmail.com>
2021-09-09 13:47:07 +0000

    prints number and list of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-import sys
-arg = sys.argv[1:]
-args = len(arg)
-index = 1
-if args is 0:
-	print("{:d} arguments.".format(args))
-elif args is 1:
-	print("{:d} aregument:".format(args))
-	print("{:d}: {:s}".format(index, sys.argv[1]))
-else:
-	print("{:d} arguments:".format(args))
-	while index <= args:
-		print("{:d}: {:s}".format(index, sys.argv[index]))
-		index += 1
+if __name__ == "__main__":
+	import sys
+	arg = sys.argv[1:]
+	args = len(arg)
+	index = 1
+	if args is 0:
+		print("{:d} arguments.".format(args))
+	elif args is 1:
+		print("{:d} aregument:".format(args))
+		print("{:d}: {:s}".format(index, sys.argv[1]))
+	else:
+		print("{:d} arguments:".format(args))
+		while index <= args:
+			print("{:d}: {:s}".format(index, sys.argv[index]))
+			index += 1
 
 	
