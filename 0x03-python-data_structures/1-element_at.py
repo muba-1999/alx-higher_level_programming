12c6336c8394097f359e14581c417355d47b0567
muba-1999 <mukobam4@gmail.com>
2021-09-13 02:41:25 -0700

    added a while loop

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    return (my_list[idx])
+    """gets an element at the given index
+    args:
+        my_list: give list
+        idx: index of an element
+    Return:
+        the element at idx
+    """
+    while idx < len(my_list):
+        if idx < 0:
+            return (None)
+        else:
+            return (my_list[idx])
+        return (None)
