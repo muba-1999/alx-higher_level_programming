086ddf8bc1a8ff299301664a70ce1003b74c68f0
muba-1999 <mukobam4@gmail.com>
2021-09-13 04:03:50 -0700

    updated code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,10 +6,14 @@ def print_matrix_integer(matrix=[[]]):
     Return:
         the matrix
     """
-    try:
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                print("{:d}".format(matrix[i][j]), end=' ')
-            print()
-    except IndexError:
+    if not matrix:
         print()
+    else:
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if j != len(matrix[i]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matrix[i][j]), end=endspace)
+            print()
