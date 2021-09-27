7a5bb368f805971163cb34c4af600087e09a66a1
muba-1999 <mukobam4@gmail.com>
2021-09-27 09:25:21 -0700

    Initial commit

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except Exception as er:
+        print("Exception:", er, file=sys.stderr)
+        return None
+    return res
---
+++ 0x05-python-exceptions/README.md
@@ -0,0 +1,1 @@
+This is a python project on exceptions and how to handle errors using try/except
