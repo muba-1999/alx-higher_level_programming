a5e7ccbe8ba5ed5bf07292be81afe7be54f49d14
muba-1999 <mukobam4@gmail.com>
2021-09-28 09:26:42 -0700

    added documentation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
+"""Documentation for a square class"""
+
 class Square:
     """Square"""
     def __init__(self, size=0):
         """
         initialize Square size
 
-        args:
-        size (int): size of the Square
-
-        Return:
-        None
+        Args:
+            size (int): size of the Square
+        Raises:
+            TypeError: When the value passed in is not an integer
+            ValueError: When the value passed in is less than 0
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        else:
+            self.__size = size
