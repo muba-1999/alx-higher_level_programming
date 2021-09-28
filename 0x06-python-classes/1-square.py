d75dca99c8f0dd156ab599f79210f18e09315d1a
muba-1999 <mukobam4@gmail.com>
2021-09-28 09:34:45 -0700

    added Documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,17 @@
+#!/usr/bin/python3
+"""Documentation for a square class"""
+
+
 class Square:
     """Square"""
     def __init__(self, size):
         """
         initializes the square attribute
 
-        args:
-        size (int): size of square
-        
+        Args:
+            size (int): size of square
+
         Return:
-        None
+            None
         """
         self.__size = size
