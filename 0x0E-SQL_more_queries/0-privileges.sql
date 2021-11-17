ecb43f59aba2bb878ea8d1644337b6f7b6ddc3f4
muba-1999 <mukobam4@gmail.com>
2021-11-17 02:25:13 -0800

    added a space

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- list the privilages of users on the mysql server
-SHOW GRANTS FOR 'user_0d_1'@'localhsot'; SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhsot;
+SHOW GRANTS FOR user_0d_2@localhost;
