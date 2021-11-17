d46296506f40621f5dc98e3a408214964afb1a09
muba-1999 <mukobam4@gmail.com>
2021-11-17 02:50:59 -0800

    initial commit

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- creates a database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE states(id UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256));
